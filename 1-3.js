db947a9fedaa6a6f16db037ba202085d972485da
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-24 22:40:10 +0100

    update single character xor

--- 1-3.js
+++ 1-3.js
@@ -4,4 +4,7 @@ var str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 // make it a byte array
 var buf = new Buffer(str, 'hex')
 
-console.log(topScore, plainText);
+var singleCharacterXOR = require('./lib/single_character_xor')
+var result = singleCharacterXOR(buf);
+
+console.log(result.plainText);
