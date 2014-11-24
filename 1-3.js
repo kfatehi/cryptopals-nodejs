fb9f64a303d3e7c6dbe468d7a18265b4ee9ac4e1
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-24 18:40:56 +0100

    broke 1 3

--- 1-3.js
+++ 1-3.js
@@ -7,32 +7,23 @@ var buf = new Buffer(str, 'hex')
 var stringScore = require('./lib/string_score')
 var xor = require('./lib/xor')
 
-function makeKey(singleByte, length) {
-  var keyBuf = [];
+function makeKey(charCode, length) {
+  var key = [];
   for (var i=0; i<length; i++) {
-    keyBuf.push(singleByte)
+    key.push(charCode)
   }
-  return new Buffer(keyBuf)
+  return new Buffer(key)
 }
 
 var alphabet = 'abcdefghijklmnopqrstuvwxyz'
 for (var i=0; i<alphabet.length; i++) {
   var letter = alphabet[i]
-  var key = new Buffer(buf.length)
-  console.log(key.toString());
+  var key = makeKey(letter.charCodeAt(), buf.length)
+  // now we have a key to try
+  // we should try it and then get the score for the result
+  // if the result scores high, it's probably english
+  var out = xor(buf, key).toString()
+  var score = stringScore(out);
+  if (score === 415)
+    console.log(out); // found you!
 }
-
-
-
-var score = stringScore(str);
-
-
-
-var key = makeKey('a', buf)
-console.log(key.toString());
-
-var out = xor(buf, key).toString()
-console.log(out);
-
-
-process.exit(1)
