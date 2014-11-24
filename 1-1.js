00e895cc703a921f3aeb6c86e6a930ae311bc1a6
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-24 14:41:17 +0100

    complete 1-1

--- 1-1.js
+++ 1-1.js
@@ -1,6 +1,4 @@
 var hex = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d"
-var out = ""
-
-
-
-require('assert').equal(out, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
+var decimal = new Buffer(hex, 'hex')
+var base64 = decimal.toString('base64')
+require('assert').equal(base64, "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t")
