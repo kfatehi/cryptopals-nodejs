2338e46bfb91946f293555e20b8767de6dfcc00a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-24 19:50:30 +0100

    my new work

--- 1-3.js
+++ 1-3.js
@@ -4,31 +4,4 @@ var str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 // make it a byte array
 var buf = new Buffer(str, 'hex')
 
-var stringScore = require('./lib/string_score')
-var xor = require('./lib/xor')
-
-function makeKey(charCode, length) {
-  var key = [];
-  for (var i=0; i<length; i++) {
-    key.push(charCode)
-  }
-  return new Buffer(key)
-}
-
-var topScore = 0;
-var plainText = 'idk';
-
-for (var i=0; i<255; i++) {
-  var key = makeKey(i, buf.length)
-  // now we have a key to try
-  // we should try it and then get the score for the result
-  // if the result scores high, it's probably english
-  var out = xor(buf, key).toString()
-  var score = stringScore(out);
-  if (score > topScore) {
-    topScore = score
-    plainText = out;
-  }
-}
-
 console.log(topScore, plainText);
---
+++ 1-4.js
@@ -0,0 +1,9 @@
+var fs = require('fs');
+var file = fs.readFileSync(__dirname+'/data/4.txt')
+
+console.log(file);
+
+file
+
+
+process.exit(1);
