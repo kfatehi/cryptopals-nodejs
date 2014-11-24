7750b7bf167ceb4dcc82102249dcaff5d581e70c
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-24 22:40:18 +0100

    use single character xor lib to solve 1 4

--- 1-4.js
+++ 1-4.js
@@ -1,9 +1,21 @@
-var fs = require('fs');
+var fs = require('fs')
+var singleCharacterXOR = require('./lib/single_character_xor')
 var file = fs.readFileSync(__dirname+'/data/4.txt')
 
-console.log(file);
+var strings = file.toString().split('\n')
 
-file
+var topScore = 0;
+var plainText = 'idk';
 
+for (var i=0; i<strings.length; i++) {
+  var str = strings[i]
+  // make it a byte array
+  var buf = new Buffer(str, 'hex')
+  var result = singleCharacterXOR(buf);
+  if (result.topScore > topScore) {
+    topScore = result.topScore
+    plainText = result.plainText;
+  }
+}
 
-process.exit(1);
+console.log(plainText);
