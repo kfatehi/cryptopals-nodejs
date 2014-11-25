774bc6ca991bacbb059a07c2fb29f8ed22864408
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-25 12:14:04 +0100

    1 5 done

---
+++ 1-5.js
@@ -0,0 +1,11 @@
+var repeatingKeyXOR = require('./lib/repeating_key_xor')
+
+var encrypted = repeatingKeyXOR(
+  "Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal",
+  "ICE"
+)
+
+var expected = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272"+
+  "a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f"
+
+require('assert').equal(encrypted, expected)
