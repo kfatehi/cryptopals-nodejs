da3ffcdddcf701a4925bfcf9095fd8601e510056
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-27 12:21:24 +0100

    clean up, comment, ready up. something clicked

--- 1-6.js
+++ 1-6.js
@@ -4,9 +4,7 @@ var keySizes = require('./lib/find_best_keysizes')(fileContent, 2, 60);
 console.log('using keysizes:', keySizes);
 process.exit(-1)
 
-// FIXME wrong, this keysize is probably not correct
-// now we know the keysize, or we think we do anyway...
-// let's chunk the file into blocks of KEYSIZE
+// chunk the file into blocks of KEYSIZE
 var numBlocks = Math.floor(fileContent.length/probablyKeySize)
 var blocks = []
 for (var i=0; i<numBlocks; i++) {
@@ -14,20 +12,12 @@ for (var i=0; i<numBlocks; i++) {
   var end = start+probablyKeySize
   blocks[i] = fileContent.slice(start, end)
 }
-
-// FIXME wrong, you should only have KEYSIZE number of blocks
-// now let's transpose the blocks, meaning:
+// transpose the blocks
 var transposed = require('./lib/transpose')(blocks, probablyKeySize, numBlocks)
-console.log('transposed block count:', transposed.length);
-
-console.log('tranposed first block length:', transposed[0].length);
-
-// now we'll solve each as though it was single-character xor
+// crack as though it is a single character xor
 var singleCharacterXOR = require('./lib/single_character_xor');
 for (var i=0; i<transposed.length; i++) {
   var block = transposed[i]
   var result = singleCharacterXOR(block)
   console.log(result.plainText);
 }
-
-
