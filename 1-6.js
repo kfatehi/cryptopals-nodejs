61d3b3888b96bc4a5cb1276cd8edeeba5804906a
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-27 13:16:23 +0100

    use lib'd file chunker, cleanup

--- 1-6.js
+++ 1-6.js
@@ -1,24 +1,18 @@
 var fileContent = require('fs').readFileSync(__dirname+'/data/4.txt')
 var keySizes = require('./lib/find_best_keysizes')(fileContent, 2, 60);
+var keyScores = []
 for (var k=0; k<5; k++) {
   var keySize = keySizes[k].keySize
+  var scoreCard = {};
   console.log('using keysize:', keySize);
-
-  // chunk the file into blocks of KEYSIZE
-  var numBlocks = Math.floor(fileContent.length/keySize)
-  var blocks = []
-  for (var i=0; i<numBlocks; i++) {
-    var start = i*keySize
-    var end = start+keySize
-    blocks[i] = fileContent.slice(start, end)
-  }
-  // transpose the blocks
-  var transposed = require('./lib/transpose')(blocks, keySize, numBlocks)
-  // crack as though it is a single character xor
+  scoreCard.keySize = keySize;
+  var blocks = require('./lib/file_chunker')(fileContent, keySize)
+  var transposed = require('./lib/transpose')(blocks, keySize, blocks.length)
+  // crack as though it is a single character xor and store results
   var singleCharacterXOR = require('./lib/single_character_xor');
   for (var i=0; i<transposed.length; i++) {
     var block = transposed[i]
     var result = singleCharacterXOR(block)
-    //console.log(result.plainText);
+    console.log(result.topScore);
   }
 }
