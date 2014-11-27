635f0837313ed1de769249e81128f480d2286647
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-27 13:37:09 +0100

    get scores

--- 1-6.js
+++ 1-6.js
@@ -1,10 +1,11 @@
+var _ = require('lodash')
 var fileContent = require('fs').readFileSync(__dirname+'/data/4.txt')
 var keySizes = require('./lib/find_best_keysizes')(fileContent, 2, 60);
 var keyScores = []
 for (var k=0; k<5; k++) {
   var keySize = keySizes[k].keySize
-  var scoreCard = {};
-  console.log('using keysize:', keySize);
+  var scoreCard = { keySize: keySize, score: 0 };
+  console.log('Analyzing keysize:', keySize);
   scoreCard.keySize = keySize;
   var blocks = require('./lib/file_chunker')(fileContent, keySize)
   var transposed = require('./lib/transpose')(blocks, keySize, blocks.length)
@@ -13,6 +14,14 @@ for (var k=0; k<5; k++) {
   for (var i=0; i<transposed.length; i++) {
     var block = transposed[i]
     var result = singleCharacterXOR(block)
-    console.log(result.topScore);
+    scoreCard.score += result.topScore;
   }
+  keyScores.push(scoreCard)
 }
+var topScores = _.sortBy(keyScores, 'score').reverse()
+console.log(topScores);
+
+var keySize = topScores[0].keySize
+console.log('Most likely keysize:', keySize);
+
+process.exit(1)
