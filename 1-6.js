102da54f50fc28c6314bcc32f9f74d8d28aae1e1
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-11-29 13:50:53 +0100

    update 1 6, still not working

--- 1-6.js
+++ 1-6.js
@@ -2,11 +2,11 @@ var _ = require('lodash')
 var fileContent = require('fs').readFileSync(__dirname+'/data/4.txt')
 var keySizes = require('./lib/find_best_keysizes')(fileContent, 2, 60);
 var keyScores = []
-for (var k=0; k<5; k++) {
+for (var k=0; k<10; k++) {
   var keySize = keySizes[k].keySize
-  var scoreCard = { keySize: keySize, score: 0 };
+  var data = { keySize: keySize, singleCharKeys: [], score: 0 };
   console.log('Analyzing keysize:', keySize);
-  scoreCard.keySize = keySize;
+  data.keySize = keySize;
   var blocks = require('./lib/file_chunker')(fileContent, keySize)
   var transposed = require('./lib/transpose')(blocks, keySize, blocks.length)
   // crack as though it is a single character xor and store results
@@ -14,14 +14,15 @@ for (var k=0; k<5; k++) {
   for (var i=0; i<transposed.length; i++) {
     var block = transposed[i]
     var result = singleCharacterXOR(block)
-    scoreCard.score += result.topScore;
+    data.score += result.topScore;
+    //data.singleCharKeys.push = result.bestKey;
   }
-  keyScores.push(scoreCard)
+  keyScores.push(data)
 }
 var topScores = _.sortBy(keyScores, 'score').reverse()
 console.log(topScores);
 
-var keySize = topScores[0].keySize
-console.log('Most likely keysize:', keySize);
+var best = topScores[0]
+console.log('Most likely keysize:', best.keySize);
 
 process.exit(1)
