7eabf9c856fd8517fc69b0284cc71dbae6a79759
Keyvan Fatehi <keyvanfatehi@gmail.com>
2014-12-11 23:04:05 +0100

    refactor 1-3

--- 1-3.js
+++ 1-3.js
@@ -5,21 +5,10 @@ var str = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
 
 // make it a byte array
 var buf = new Buffer(str, 'hex')
+var singleByteXOR = require('./lib/xor').xor_singlebyte
 
-var singleCharacterXOR = require('./lib/xor').xor_singlebyte
-var result = singleCharacterXOR(buf);
+var crackSingleByteXORCipher = require('./lib/crack_single_byte_xor_cipher')
+var plain = crackSingleByteXORCipher(buf);
 
-bestScore = 10000;
-bestIx = 0;
-for (var i = 0; i < 256; i++){
-	var result = singleCharacterXOR(buf, i);
-	var b = new Buffer(result);
-	var score = string_score(b.toString());
-	if (score < bestScore){
-		bestScore = score;
-		bestIx = i;
-	}
-}
-
-buf = new Buffer(singleCharacterXOR(buf, bestIx));
-console.log(buf.toString());
+console.log(plain)
+require('assert').equal(plain, "Cooking MC's like a pound of bacon")
